e4169e94f878980758f7f83e975b2f7d5c8a419c
XIUXIUXIU5 <shaolei@ucla.edu>
2016-01-05 10:08:59 -0800

    init

---
+++ actors.sql
@@ -0,0 +1,4 @@
+create table Actors(Name VARCHAR(40), Movie VARCHAR(80), Year INTEGER, Role VARCHAR(40));
+LOAD DATA LOCAL INFILE "~/data/actors.csv" INTO TABLE Actors FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+select Name from Actors where Movie='Die Another Day';
+drop table Actors;

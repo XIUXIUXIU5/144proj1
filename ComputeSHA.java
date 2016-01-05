10c42f54924f4eefea9591847fc3784333ec2725
XIUXIUXIU5 <shaolei@ucla.edu>
2016-01-05 10:13:28 -0800

    format

--- ComputeSHA.java
+++ ComputeSHA.java
@@ -5,16 +5,15 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class ComputeSHA {
-	public static void main(String [ ] args) throws NoSuchAlgorithmException, IOException
-	{
-		  byte[] encoded = Files.readAllBytes(Paths.get(args[0]));
-		 MessageDigest md = MessageDigest.getInstance("SHA");
-		 md.update(encoded);
-		 byte[] rawBytes = md.digest();
-	     StringBuilder sb = new StringBuilder();
-	        for (int i = 0; i < rawBytes.length; i++) {
-	          sb.append(String.format("%02x",rawBytes[i]));
-	        }
-	     System.out.println(sb.toString());
-	}	
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(args[0]));
+		MessageDigest md = MessageDigest.getInstance("SHA");
+		md.update(encoded);
+		byte[] rawBytes = md.digest();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < rawBytes.length; i++) {
+			sb.append(String.format("%02x", rawBytes[i]));
+		}
+		System.out.println(sb.toString());
+	}
 }
